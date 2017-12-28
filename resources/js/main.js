e655bde41caeedbec37dedf8031875501d6322ff
tbeck10 <trbeck10@gmail.com>
2017-12-28 14:29:48 -0500

    Add carousel functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-  $(".navbar-nav a").on("click", function(event){
+  //Offset navbar when clicking anchor
+
+  $(".navbar a").on("click", function(event){
     event.preventDefault();
     var navHeight = $("nav").height();
     var id = $(this).attr("href");
@@ -9,6 +11,32 @@ $(document).ready(function() {
     $("html").animate({
       scrollTop : newScrollCoordinate
     });
+  });
+
+  //Offset & animate jumbotron anchor
+
+  $(".jumbotron a").on("click", function(event){
+    event.preventDefault();
+    var navHeight = $("nav").height();
+    var id = $(this).attr("href");
+    var originalScrollCoordinate = $(id).offset().top;
+    var newScrollCoordinate = originalScrollCoordinate - navHeight;
+    $("html").animate({
+      scrollTop : newScrollCoordinate
+    });
+  });
+
+  //Slick carousel
+
+  $('.carousel').slick({
+    slidesToShow: 1,
+    SlidestoScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    pauseOnHover: false,
+    draggable: false,
+    pauseOnFocus: false,
   });
 
 });
